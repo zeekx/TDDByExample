ffce61459c5ec870343f4de75aaa397119a61a2f
yubinqiang <yubinqiang@51talk.com>
2018-07-18 19:52:55 +0800

    setUp

--- xUnit/TestCaseTests.py
+++ xUnit/TestCaseTests.py
@@ -3,11 +3,18 @@ from TestCase import TestCase
 # import unittest
 
 class TestCaseTests(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
+    def testSetUp(self):
+        self.test.run()
+        assert(self.test.wasSetUp)
+
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        #assert(not test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
 
 
 TestCaseTests("testRunning").run()
+TestCaseTests("testSetUp").run()
