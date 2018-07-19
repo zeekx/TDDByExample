051cfaa8395e1d20e1f6943837bfc451937e4791
yubinqiang <yubinqiang@51talk.com>
2018-07-19 11:14:34 +0800

    1.Delete testRunning. 2.change testSetUp to testTemplateMethod

--- xUnit/TestCaseTests.py
+++ xUnit/TestCaseTests.py
@@ -3,18 +3,9 @@ from TestCase import TestCase
 # import unittest
 
 class TestCaseTests(TestCase):
-    def setUp(self):
+    def testTemplateMethod(self):
         self.test = WasRun("testMethod")
-
-    def testSetUp(self):
         self.test.run()
         assert("setUp testMethod " == self.test.log)
 
-    def testRunning(self):
-        #assert(not test.wasRun)
-        self.test.run()
-        assert(self.test.wasRun)
-
-
-TestCaseTests("testRunning").run()
-TestCaseTests("testSetUp").run()
+TestCaseTests("testTemplateMethod").run()
